30238b637cb82061d27d8768ad3d24e58f0c2309
sagarbisht07 <bishtsagarbisht1232gmail.com>
2022-09-20 21:26:19 +0530

    i fixed the prime code

--- prime.js
+++ prime.js
@@ -4,8 +4,11 @@ function prime(num){
         if(num%i==0)
         count++;
     }
-    if(count>2){
+    if(count==2){
         return true;
     }
     return false;
-}
+} 
+let x= prime(13);
+if(x==true) console.log("num is prime");
+else  console.log("num is not prime");
